07c26a285f9772c0df570eaf35f46e215f57fb66
alanoud <alanoudmohammed958@outlook.com>
2021-12-13 12:17:03 +0300

    somthing added

--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -1,23 +1,73 @@
-const express = require('express')
-const User = require('../models/user')
+const express = require("express");
+const User = require("../models/user");
+const Parking = require("../models/parking");
+const booking = require("../models/booking");
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.use(express.json())
+userRouter.use(express.json());
 // userRouter.use(express.urlencoded({extended: false}))
 
-userRouter.get('/user', async (req,res)=>{
-    const user = await User.find()
-    res.send(user)
+userRouter.get("/user", async (req, res) => {
+  const user = await User.find();
+  res.send(user);
+});
+
+userRouter.put("/updateUser/:id", async (req, res) => {
+  const updateUser = await User.findByIdAndUpdate(req.params.id, {
+    $set: req.body,
+  });
+  res.send(updateUser);
+});
+userRouter.delete("/deleteUser/:id", async (req, res) => {
+  const deleteUser = await User.findOneAndRemove(req.params.id);
+  res.send(deleteUser);
+});
+
+userRouter.get("/allParking", async (req, res) => {
+    const parkings = await Parking.find()
+    res.send(parkings)
+  })
+
+userRouter.post("/userListOfParking", async (req,res)=>{
+     let userId = req.body.id
+     console.log(req.body.id);
+     try{
+         const userParking = await booking.find({"user":userId})
+         .then((booking)=>{
+
+             res.send(booking)
+         })
+
+     }catch(e){
+         res.send(e)
+     }
+    
 })
 
-userRouter.put('/updateUser/:id', async (req,res)=>{
-    const updateUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body })
-    res.send(updateUser)
-})
-userRouter.delete('/deleteUser/:id', async (req,res)=>{
-    const deleteUser = await User.findOneAndRemove(req.params.id)
-    res.send(deleteUser)
-})
+userRouter.post("/bookingParking", (req, res) => {
+  Parking.findById(req.body.parking).then(async(parking) => {
+    if (parking.numberOfParking > 0) {
+        parking.numberOfParking --;
+        await parking.save();
+      booking
+        .create({
+          parking: req.body.parking,
+          user: req.body.user,
+          startTime: req.body.startTime,
+        })
+        .then(async (newBooking) => {
+          try {
+            await newBooking.save();
+            res.send(newBooking);
+          } catch (error) {
+            res.send(error);
+          }
+        });
+    } else {
+      res.send("sorry parking is full");
+    }
+  });
+});
 
-module.exports = userRouter
+module.exports = userRouter;
--- backend/models/booking.js
+++ backend/models/booking.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 var Schema = mongoose.Schema
+const moment = require('moment-timezone');
+
+const timeZone = moment.tz.guess(true)
+const KSAdate = moment().utcOffset(0, true).format()
+
 
 const bookingSchema = new mongoose.Schema({
     parking:{
@@ -12,15 +17,18 @@ const bookingSchema = new mongoose.Schema({
     },
     startTime:{
         type:Date,
+        default: KSAdate
     },
     endTime:{
         type:Date,
+        
     },
     price:{
         type:Number,
     },
     date:{
         type:Date,
+        default: KSAdate
     }
 })
 
