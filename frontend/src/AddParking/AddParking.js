28bcba5b362c2bf4afd12b53821c3fbfcdddc3ac
Alanoudalmakhalafi <alanoudm540@gmail.com>
2021-12-14 21:02:48 +0300

    yes yes adding parking done

--- frontend/src/AddParking/AddParking.js
+++ frontend/src/AddParking/AddParking.js
@@ -1,42 +1,44 @@
-import React, {useRef} from 'react'
+import React, { useRef, useState } from "react";
 import axios from "axios";
 
-
 export default function AddParking() {
+  const [NewParking, setNewParking] = useState();
 
-    const latitude = useRef(null)
-      const longitude = useRef(null)
-      const num = useRef(null)
-      const img = useRef(null)
-      const price = useRef(null)
+  const latitude = useRef(null);
+  const longitude = useRef(null);
+  const num = useRef(null);
+  const img = useRef(null);
+  const price = useRef(null);
 
-  
-
-    const handleClickEvent = (e) => {
-      e.preventDefault();
-      axios
+  const handleClickEvent = (e) => {
+    e.preventDefault();
+    axios
       .post(" http://localhost:3001/admin/addParking", {
-        
-        latitude=latitude.current.value,
-        longitude=longitude.current.value,
-        num=num.current.value,
-        img=img.current.value,
-        price=price.current.value,
-    }
-        )
-       
+        latitude: latitude.current.value,
+        longitude: longitude.current.value,
+        numberOfParking: num.current.value,
+        image: img.current.value,
+        price: price.current.value,
+      })
+      .then(
+        (res) => {
+        console.log(res);
+      },
+      (err)=> {
+        console.log(err)
       }
+      );
+  };
 
-      return (
-        <div>
-    
-          <input ref={latitude} placeholder='latitude' required />
-          <input ref={longitude} placeholder='longitude' required />
-          <input ref={num} placeholder='number of parking' required />
-          <input ref={img} placeholder='image' required />
-          <input ref={price} placeholder='parking price' required />
+  return (
+    <div>
+      <input ref={latitude} placeholder="latitude" required />
+      <input ref={longitude} placeholder="longitude" required />
+      <input ref={num} placeholder="number of parking" required />
+      <input ref={img} placeholder="image" required />
+      <input ref={price} placeholder="parking price" required />
 
-          <button onClick={handleClickEvent(e)}>Get value</button>
-        </div>
-      )
+      <button onClick={handleClickEvent}>Get value</button>
+    </div>
+  );
 }
