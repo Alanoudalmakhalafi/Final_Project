26217541ed7ada18d92ce6e5e2e00115629a91e3
Alanoudalmakhalafi <alanoudm540@gmail.com>
2021-12-16 12:35:14 +0300

    homeSidebar functionality

--- frontend/src/Home/HomeSidebar.js
+++ frontend/src/Home/HomeSidebar.js
@@ -1,46 +1,60 @@
-import React,  {useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
+import "react-calendar/dist/Calendar.css";
 import axios from "axios";
-
+import Calendar from "react-calendar";
 
 export default function HomeSidebar() {
-
-  const [parkings, setParkings] = useState()
-  const [Loding, setLoding] = useState(true)
-
+  const [parkings, setParkings] = useState();
+  const [Loding, setLoding] = useState(true);
+  const [value, onChange] = useState(new Date());
 
   useEffect(() => {
     axios.get(" http://localhost:3001/user/allParking").then((res) => {
-      console.log(res.data)
-      setParkings(res.data)
-      setLoding(false)
-
-    })
-  }, [])
+      console.log(res.data);
+      setParkings(res.data);
+      setLoding(false);
+    });
+  }, []);
 
   //loding
-  if (Loding) {return <p>Loding</p>}
+  if (Loding) {
+    return <p>Loding</p>;
+  }
 
   return (
     <div className="homeSidebar">
       <ProSidebar>
         <Menu iconShape="square">
+          <MenuItem>
+            <input className="searchBar" placeholder="Search" />
+          </MenuItem>
+          <SubMenu>
+            <Calendar
+              onChange={onChange}
+              value={value}
+              calendarType="Arabic"
+              locale="en-US"
+            />
+            <p className="text-center">
+              <span className="bold">Selected Date:</span>{" "}
+              {value.toDateString()}
+            </p>
+          </SubMenu>
 
-          <MenuItem><input className="searchBar" placeholder="Search"/></MenuItem>
-          {parkings.map((p)=>{
-            return(
+          {parkings.map((p) => {
+            return (
               <SubMenu title={p.StreetName}>
-              <div className="">
-              <img src={p.image} height="200px" width="200px" />
-              <p>{p.StreetName}</p>
-              <p>number Of Parking : {p.numberOfParking}</p>
-              <p>price : {p.price}</p>
-            </div>
+                <div className="">
+                  <img src={p.image} height="200px" width="200px" />
+                  <p>{p.StreetName}</p>
+                  <p>number Of Parking : {p.numberOfParking}</p>
+                  <p>price : {p.price}</p>
+                </div>
               </SubMenu>
-            )
+            );
           })}
-          
         </Menu>
       </ProSidebar>
     </div>
