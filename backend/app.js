50d96cd26994f1d3e6b8098c3d339c94f41f4ff6
Alanoudalmakhalafi <alanoudm540@gmail.com>
2022-01-09 18:13:51 +0300

    working on user side

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,12 @@ const userRouter = require('./Routes/userRoute')
 const adminRouter = require('./Routes/adminRoute')
 const authRoutes = require('./Routes/authRoute')
 const cors = require('cors')
+const stripe = require("stripe")(
+  "sk_test_51KG1ymCh49zQHfRHdQKGKg994Hfan2sDrZqVUAKbsdq2YuRhVJBD5mWXPDMlth3oYaBrBpzRg3sw1EjIcsySc0s700zoEXKwEc"
+);
+
+
+
 
 const app = express()
 
@@ -11,6 +17,25 @@ app.use(cors())
 app.use('/user', userRouter)
 app.use('/admin', adminRouter)
 app.use(authRoutes)
+
+app.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+
+
 
 
 const uri = 'mongodb+srv://alanoud:1418@cluster0.anylu.mongodb.net/parkingSite?retryWrites=true&w=majority';
--- frontend/src/userProfile/Checkout.js
+++ frontend/src/userProfile/Checkout.js
@@ -1,57 +1,105 @@
-import React,{useEffect, useState} from 'react'
-import axios from 'axios';
-import './bookingList.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./bookingList.css";
+import StripeCheckout from "react-stripe-checkout";
 
+export default function Checkout({ e, setRefresh }) {
+  const [isClicked, setIsClicked] = useState(true);
 
+  const checkOut = (id) => {
+    axios
+      .put(`http://localhost:3001/user/checkOut`, {
+        bookingId: id,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setRefresh(true);
 
-export default function Checkout({ e , setRefresh}) {
-    const [isClicked, setIsClicked] = useState(true)
+        axios
+          .post("/orders/create", { userId: decodedData.id })
 
-    const checkOut = (id) =>{
-        axios.put(`http://localhost:3001/user/checkOut`,{
-              bookingId:id
-          }).then((res) => {
-              console.log(res.data)
-              setRefresh(true)
+          .then(async (res) => {
+            try {
+              const res = await axios.post("/payment", {
+                tokenId: token.id,
+                amount: total * 3.75 * 100,
+              });
+            } catch (error) {}
+
+            console.log(res);
           })
-      }
+          .catch(function (error) {
+            console.log(error);
+          });
+      });
+  };
 
-
-    const showCheckoutBox = () => {
-        if (isClicked) {
-          setIsClicked(false);
-        } else {
-          setIsClicked(true);
-        }
-      }; 
+  const showCheckoutBox = () => {
+    if (isClicked) {
+      setIsClicked(false);
+    } else {
+      setIsClicked(true);
+    }
+  };
   return (
     <>
-      <div className='bookingList'>
-                {isClicked ? (<>
+      <div className="bookingList">
+        {isClicked ? (
+          <>
+            <div className="bookingInfo">
+              <p>{e.parking.StreetName}</p>
+              <p>price per hour :</p>
+              <p>{e.parking.price} SR</p>
+              <p>start time :</p>
+              <p>{e.startTime}</p>
 
-                <div className='bookingInfo'>
-                    <p>{e.parking.StreetName}</p>
-                    <p>price per hour :</p>
-                    <p>{e.parking.price} SR</p>
-                    <p>start time :</p>
-                    <p>{e.startTime}</p>
-                    {e.IsChecked ? "":
-                    
-                    <button onClick={()=>{checkOut(e._id); setIsClicked(false)}}>Checkout</button>
-                    }
-                    </div>
-                </>) : (<>
+              <StripeCheckout
+                stripeKey="pk_test_51KG1ymCh49zQHfRHvoXz0OPDR2AmP7ypTJIvpvIuMr2PaKkfJe1Qs8LAWxPg3ZQ09a4Xkn2bAvBICs5rm57O2Sdm00GNCBQN0W"
+                token={checkOut(e._id)}
+                billingAddress
+                shippingAddress
+                amount={e.totalPrice}
+                // name={cartItems}
+              >
+                <button
+                  onClick={() => {
+                    setIsClicked(false);
+                  }}
+                >
+                  Checkout
+                </button>
+              </StripeCheckout>
 
-                    <div className='checkoutInfo'>
-                    <p>total Price :</p>
-                    <p>{e.totalPrice} SR</p>
-                    <p>end time :</p>
-                    <p>{e.endTime}</p>
-                    </div>
-                </>)}
-                    </div>
-                    
-      
+              {e.IsChecked ? (
+                ""
+              ) : (
+                <>
+                  {/* <StripeCheckout
+                      stripeKey="pk_test_51KG1ymCh49zQHfRHvoXz0OPDR2AmP7ypTJIvpvIuMr2PaKkfJe1Qs8LAWxPg3ZQ09a4Xkn2bAvBICs5rm57O2Sdm00GNCBQN0W"
+                      token={checkOut(e._id)}
+                      billingAddress
+                      shippingAddress
+                      amount={e.totalPrice}
+                      // name={cartItems}
+                    >
+                      <button onClick={()=>{setIsClicked(false)}}>Checkout</button>
+                    </StripeCheckout> */}
+                  {/* <button onClick={()=>{checkOut(e._id); setIsClicked(false)}}>Checkout</button> */}
+                </>
+              )}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="checkoutInfo">
+              <p>total Price :</p>
+              <p>{e.totalPrice} SR</p>
+              <p>end time :</p>
+              <p>{e.endTime}</p>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
